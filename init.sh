fac70dc0f86f88d70f5bb8c18363c2d996b1408e
stalkertok <stalkertok@gmail.com>
2016-03-09 22:22:24 +0800

    Signed-off-by: stalkertok <stalkertok@gmail.com>

--- init.sh
+++ init.sh
@@ -2,4 +2,3 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ﻿gunicorn -b 0.0.0.0:8080 hello:app﻿
-sudo /etc/init.d/gunicorn restart
