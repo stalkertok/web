ddb978561ab74972f37ace163bba399b969fdbaa
stalkertok <stalkertok@gmail.com>
2016-03-09 20:53:01 +0800

    Signed-off-by: stalkertok <stalkertok@gmail.com>

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo rm /etc/nginx/sites-enabled/default
-ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
