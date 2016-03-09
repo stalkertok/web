0a2788f5cc6de3e58f79d21e5deb01a20923304b
stalkertok <stalkertok@gmail.com>
2016-03-09 22:20:56 +0800

    Signed-off-by: stalkertok <stalkertok@gmail.com>

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
- def app (environ, start_response):
+def app (environ, start_response):
   status = '200 OK'
   response_headers = [('Content-type','text/plain')]
   start_response(status, response_headers)
