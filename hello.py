aa009dc214281c152687ef7641e2f07cf87f1c8c
stalkertok <stalkertok@gmail.com>
2016-03-09 22:45:38 +0800

    Signed-off-by: stalkertok <stalkertok@gmail.com>

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
- def app (environ, start_response):
-  status = '200 OK'
-  response_headers = [('Content-type','text/plain')]
-  start_response(status, response_headers)
-  resp = environ['QUERY_STRING'].split("&")
-  resp = [item+"\r\n" for item in resp]
-  return resp
+def app(env, start_response):
+	status = '200 OK'
+	response_headers = [('Content-Type', 'text/plain')]
+	start_response(status, response_headers)
+	response = env['QUERY_STRING'].split("&")
+	response = [item+"\r\n" for item in response]
+	return response
